48750cea21b7e088e6919314d962aae38fae1633
genilton10 <geniltonlisboa@hotmail.com>
2023-04-13 17:26:48 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lisboa/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lisboa.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lisboa.workshopmongo.domain.Post;
+import com.lisboa.workshopmongo.repositories.PostRepository;
+import com.lisboa.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
